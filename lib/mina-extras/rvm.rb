37abbc9c168c989b1deb86f8d752acda30e42f20
Jan Berdajs <mrbrdo@gmail.com>
2013-11-23 19:40:58 +0100

    handle case where gemset is in .ruby-version

--- lib/mina-extras/rvm.rb
+++ lib/mina-extras/rvm.rb
@@ -1,7 +1,13 @@
 app_root = %x[pwd].strip
 
-set :current_ruby, File.read("#{app_root}/.ruby-version").strip
-set :current_gemset, File.read("#{app_root}/.ruby-gemset").strip
+version = File.read("#{app_root}/.ruby-version").strip
+if File.exist?("#{app_root}/.ruby-gemset")
+  gemset = File.read("#{app_root}/.ruby-gemset").strip
+else
+  version, gemset = version.split("@")
+end
+set :current_ruby, version
+set :current_gemset, gemset
 
 task :'rvm:use:current' do
   invoke :"rvm:use[#{current_ruby!}@#{current_gemset!}]"
