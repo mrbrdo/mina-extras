9ffd886bb9aa101e1f325af76dd112c6fa025539
Jan Berdajs <mrbrdo@gmail.com>
2014-11-08 07:21:14 +0100

    make puma more configurable

--- lib/mina-extras/puma.rb
+++ lib/mina-extras/puma.rb
@@ -1,17 +1,18 @@
 namespace :puma do
   task :config do
-    cmd_prefix  = "cd #{deploy_to}/#{current_path} ; "
-    cmd_prefix += "RAILS_ENV=#{rails_env!} NEW_RELIC_DISPATCHER=puma bundle exec"
-    set :puma_start_cmd, "#{cmd_prefix} puma -C #{File.join(deploy_to,current_path,'config','puma.rb')}"
-    set :puma_stop_cmd, "#{cmd_prefix} pumactl -S #{deploy_to}/#{shared_path}/sockets/puma.state stop"
-    set :puma_restart_cmd, "#{cmd_prefix} pumactl -S #{deploy_to}/#{shared_path}/sockets/puma.state phased-restart"
+    set :puma_cmd_prefix, "cd #{deploy_to}/#{current_path} ; RAILS_ENV=#{rails_env!}"
+    set :puma_pid_path, "#{deploy_to}/#{shared_path}/pids/puma.pid"
+    set :puma_sock_dir, "#{deploy_to}/#{shared_path}/sockets"
+    set :puma_start_cmd, "bundle exec puma -C #{File.join(deploy_to,current_path,'config','puma.rb')}"
+    set :puma_stop_cmd, "bundle exec pumactl -S #{puma_sock_dir}/puma.state stop"
+    set :puma_restart_cmd, "bundle exec pumactl -S #{puma_sock_dir}/puma.state phased-restart"
   end
 
   desc 'Start puma'
   task :start => [:config, :environment] do
     queue %{
       echo "-----> Starting puma"
-      #{echo_cmd puma_start_cmd}
+      #{echo_cmd "#{puma_cmd_prefix} #{puma_start_cmd}"}
     }
   end
 
@@ -19,23 +20,23 @@ namespace :puma do
   task :stop => [:config, :environment] do
     queue %{
       echo "-----> Stopping puma"
-      #{echo_cmd puma_stop_cmd}
+      #{echo_cmd "#{puma_cmd_prefix} #{puma_stop_cmd}"}
     }
   end
 
   desc 'Restart puma'
   task :restart => [:config, :environment] do
     queue %{
-      PUMA_PID=$(cat "#{deploy_to}/#{shared_path}/pids/puma.pid")
+      PUMA_PID=$(cat "#{puma_pid_path}")
       kill -0 $PUMA_PID > /dev/null 2>&1
       if [ $? != 0 ]; then
-        rm "#{deploy_to}/#{shared_path}/sockets/pumactl.sock" > /dev/null 2>&1
-        rm "#{deploy_to}/#{shared_path}/sockets/puma.sock" > /dev/null 2>&1
+        rm "#{puma_sock_dir}/pumactl.sock" > /dev/null 2>&1
+        rm "#{puma_sock_dir}/puma.sock" > /dev/null 2>&1
         echo "-----> Starting puma"
-        #{echo_cmd puma_start_cmd}
+        #{echo_cmd "#{puma_cmd_prefix} #{puma_start_cmd}"}
       else
         echo "-----> Restarting puma"
-        #{echo_cmd puma_restart_cmd}
+        #{echo_cmd "#{puma_cmd_prefix} #{puma_restart_cmd}"}
       fi
     }
   end
